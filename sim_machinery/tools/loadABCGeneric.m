49792febe5fdf7e74248cbbec118d70440b6ae6b
twestWTCN <timothy.west.10@ucl.ac.uk>
2022-01-14 12:57:36 +0000

    Update loadABCGeneric.m

--- sim_machinery/tools/loadABCGeneric.m
+++ sim_machinery/tools/loadABCGeneric.m
@@ -1,4 +1,8 @@
 function varargout = loadABCGeneric(R,varname)
+if ~iscell(varname)
+    warning('Varnames must be given as cell array!')
+    varname = {varname};
+end
 for i = 1:numel(varname)
     load([R.path.rootn '\outputs\' R.path.projectn '\'  R.out.tag '\' R.out.dag '\' varname{i} '_' R.out.tag '_' R.out.dag '.mat'])
     varargout{i} = varo;
