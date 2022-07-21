31c5bbb3f6e376f097e1175ab7ef3ff104cbdab8
twestWTCN <timothy.west.10@ucl.ac.uk>
2022-07-21 12:02:04 +0100

    Update computeFeatureAverage.m

--- sim_machinery/graph_outputs/computeFeatureAverage.m
+++ sim_machinery/graph_outputs/computeFeatureAverage.m
@@ -1,4 +1,7 @@
 function feat_stat = computeFeatureAverage(R,feat_rep)
+st = dbstack;
+namestr = st.name;
+warning(['Function "' namestr '.m" is only designed for your burstABC feature order and IS NOT GENERIC!'])
 % this function compiles all of the model draws and finds statistics
 % feat_stat is organized as [mean std sem iqr]
 feat_stat = [];
@@ -6,18 +9,18 @@ for feat = 1:numel(R.data.datatype)
     Xtmp = [];
     % now collect terms of feature
     for rep = 1:numel(feat_rep)
+
+
         if ~any(isnan(feat_rep{rep}{1}(:)))
-            if ndims(feat_rep{rep}{feat})>2
-                Xtmp(:,:,:,:,:,rep) = feat_rep{rep}{feat}(1,1,1,R.siminds,:);
-            elseif ~any(size(feat_rep{rep}{feat})==1)
-                Xtmp(:,:,:,:,:,rep) = feat_rep{rep}{feat}(1,:);
+            if  feat == 1
+                Xtmp(:,:,rep) = feat_rep{rep}{feat}(R.siminds,:);
             else
-                Xtmp(:,:,rep) = feat_rep{rep}{feat};
+                Xtmp(:,:,rep) = feat_rep{rep}{feat}(:,1)';
             end
             
         else % fill with nans
-            if feat==1
-                Xtmp(:,:,:,:,:,rep) = nan(1,1,1,1,size(R.data.feat_xscale{feat},2));
+            if  feat == 1
+                Xtmp(:,:,rep) = nan(1,1,size(R.data.feat_xscale{feat},2));
             else
                 Xtmp(:,:,rep) = nan(1,size(R.data.feat_xscale{feat},2));
             end
