fdbb482c28df8b4b85d3c4884a022d15af69d1e3
twestWTCN <timothy.west.10@ucl.ac.uk>
2022-10-27 10:37:18 +0100

    CommentBurstPlots

--- sim_machinery/datafeatures/burstAmpHist.m
+++ sim_machinery/datafeatures/burstAmpHist.m
@@ -32,17 +32,17 @@ if nargout>2
     end
 end
 
-if nargin>6
-    plotBurstConstruction(fsamp,dataC,dataX,XH,burstinds)
-    figure
-    H = histogram(amp,bins,'Normalization','pdf'); hold on
-    H.FaceColor = [131 131 131]/256;
-    plot(nb,Apdf,'LineWidth',1.5,'Color',H.FaceColor.*0.4)
-    
-    a = gca;
-        a.YTickLabel = {};
-   a.Color = 'none';
-   box off; axis square
-    xlabel('Amplitude (Z)')
-    ylabel('pdf')
-end
+% if nargin>6
+%     plotBurstConstruction(fsamp,dataC,dataX,XH,burstinds)
+%     figure
+%     H = histogram(amp,bins,'Normalization','pdf'); hold on
+%     H.FaceColor = [131 131 131]/256;
+%     plot(nb,Apdf,'LineWidth',1.5,'Color',H.FaceColor.*0.4)
+%     
+%     a = gca;
+%         a.YTickLabel = {};
+%    a.Color = 'none';
+%    box off; axis square
+%     xlabel('Amplitude (Z)')
+%     ylabel('pdf')
+% end
